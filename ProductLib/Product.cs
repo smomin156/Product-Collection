ede3cdc25be9f9634616a27dfc3330d338c721b5
smomin156 <63006524+smomin156@users.noreply.github.com>
2021-08-05 19:06:13 +0530

    Add files via upload

---
+++ ProductLib/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductLib
+{
+    public class Product
+    {
+        #region Fields
+        private string productName;
+        private double unitPrice;
+        private int quantity;
+        private double total;
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+        public double UnitPrice
+        {
+            get { return unitPrice; }
+            set { unitPrice = value; }
+        }
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        } 
+        public double Total
+        {
+            get { return UnitPrice * Quantity; }
+            private set { total = value; }
+        }
+        #endregion
+        #region Constructor
+        public Product(string name, double price, int quantity)
+        {
+            this.ProductName = name;
+            this.UnitPrice = price;
+            this.Quantity = quantity;
+        }
+        #endregion
+        public override string ToString()
+        {
+            return string.Format($"Product Name:{ProductName} Unit Price:{UnitPrice} Quantity:{Quantity} Total:{Total}");
+        }
+    }
+}
---
+++ ProductLib/ProductCollection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductLib
+{
+    public  class ProductCollection
+    {
+        private  List<Product> products;
+         public ProductCollection()
+        {
+            products = new List<Product>();
+        }
+
+        public  void AddProduct(Product obj)
+        {
+            products.Add(obj);
+        }
+        public  void DeleteProduct(string name)
+        {
+            var itemRemove = products.Single(p => p.ProductName == name);
+            products.Remove(itemRemove);
+        }
+        public  Product[] DisplayAll()
+        {
+            return products.ToArray();
+        }
+        public Product Search(string name)
+        {
+            var item= products.Single(p => p.ProductName == name);
+            return item;
+        }
+        public Product[] QueryUnitPrice(double unitPrice,char ch)
+        {
+            List<Product> pArray = new List<Product>();
+            switch (ch)
+            {
+                case '>':
+                    pArray = (products.FindAll(p => p.UnitPrice > unitPrice));
+                    break;
+                case '<':
+                    pArray = (products.FindAll(p => p.UnitPrice < unitPrice));
+                    break;
+                case '=':
+                    pArray = (products.FindAll(p => p.UnitPrice == unitPrice));
+                    break;
+                default:
+                    break;
+
+            }
+            return pArray.ToArray();
+        }
+
+    }
+ 
+
+
+}
+
